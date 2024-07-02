8c8d07d9354a9d7765ae6a86bef672f3697e453c
nicolasmanz <nicolas@startreming.com>
2024-07-02 11:36:41 -0300

    Improved dirt particles, added boost particle

--- Assets/Scripts/Car/CarParticles.cs
+++ Assets/Scripts/Car/CarParticles.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace Car
@@ -5,8 +6,9 @@ namespace Car
     public class CarParticles : MonoBehaviour
     {
         [Header("Particle Prefabs")]
-        [SerializeField] private ParticleSystem smokeParticle;
+        [SerializeField] private ParticleSystem[] smokeParticles;
         [SerializeField] private ParticleSystem[] wheelDirtParticles;
+        [SerializeField] private ParticleSystem boostParticle;
 
         private CarController _kartController;
         private float _wheelDirtParticleMultiplier = 10f;
@@ -15,6 +17,17 @@ namespace Car
         private void Awake()
         { 
             _kartController = GetComponent<CarController>();
+            _kartController.OnBoostStart += BoostStart;
+        }
+
+        private void OnDestroy()
+        {
+            _kartController.OnBoostStart -= BoostStart;
+        }
+
+        private void BoostStart()
+        {
+            boostParticle.Play();
         }
 
         private void Update()
@@ -39,13 +52,16 @@ namespace Car
         
         private void AdjustSmokeParticle()
         {
-            float rateOverTimeValue;
+            foreach (var smokeParticle in smokeParticles)
+            {
+                float rateOverTimeValue;
             
-            if (!_kartController.Grounded) rateOverTimeValue = 0f;
-            else rateOverTimeValue = _kartController.CurrentSpeed * _smockeParticleMultiplier;
+                if (!_kartController.Grounded) rateOverTimeValue = 0f;
+                else rateOverTimeValue = _kartController.CurrentSpeed * _smockeParticleMultiplier;
             
-            var emission = smokeParticle.emission;
-            emission.rateOverTime = rateOverTimeValue;
+                var emission = smokeParticle.emission;
+                emission.rateOverTime = rateOverTimeValue;
+            }
         }
     }
 }
